9ba83e1d11af2c38f970ec95fb5cfca1db5e7e5a
fahmikudo <fahmi.hidayatullah12@gmail.com>
2022-04-02 07:22:51 +0700

    update, delete, and get product

--- app/controllers/product/ProductMerchantController.java
+++ app/controllers/product/ProductMerchantController.java
@@ -1,16 +1,17 @@
 package controllers.product;
 
 import com.avaje.ebean.Ebean;
+import com.avaje.ebean.Query;
 import com.avaje.ebean.Transaction;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hokeba.api.BaseResponse;
 import controllers.BaseController;
+import dtos.product.ProductDescriptionResponse;
+import dtos.product.ProductDetailResponse;
 import dtos.product.ProductRequest;
-import models.BrandMerchant;
-import models.CategoryMerchant;
-import models.Merchant;
-import models.SubCategoryMerchant;
+import dtos.product.ProductResponse;
+import models.*;
 import models.merchant.ProductMerchant;
 import models.merchant.ProductMerchantDescription;
 import models.merchant.ProductMerchantDetail;
@@ -19,6 +20,9 @@ import play.libs.Json;
 import play.mvc.Result;
 import repository.*;
 import validator.ProductValidator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductMerchantController extends BaseController {
 
@@ -59,14 +63,14 @@ public class ProductMerchantController extends BaseController {
                     newProductMerchantDetail.save();
 
                     ProductMerchantDescription newProductMerchantDescription = new ProductMerchantDescription();
-                    newProductMerchantDescription.setShortDescription(productRequest.getShortDescription());
-                    newProductMerchantDescription.setLongDescription(productRequest.getLongDescription());
+                    newProductMerchantDescription.setShortDescription(productRequest.getProductDescriptionRequest().getShortDescription());
+                    newProductMerchantDescription.setLongDescription(productRequest.getProductDescriptionRequest().getLongDescription());
                     newProductMerchantDescription.setProductMerchantDetail(newProductMerchantDetail);
                     newProductMerchantDescription.save();
 
                     trx.commit();
 
-                    response.setBaseResponse(1,offset, 1, success + " Product updated successfully", null);
+                    response.setBaseResponse(1,offset, 1, success + " Product updated successfully", newProductMerchant.id);
                     return ok(Json.toJson(response));
 
                 } catch (Exception e) {
@@ -98,7 +102,7 @@ public class ProductMerchantController extends BaseController {
                     response.setBaseResponse(0, 0, 0, validateRequest, null);
                     return badRequest(Json.toJson(response));
                 }
-                ProductMerchant productMerchant = ProductMerchantRepository.findById(id);
+                ProductMerchant productMerchant = ProductMerchantRepository.findById(id, ownMerchant);
                 if (productMerchant == null) {
                     response.setBaseResponse(0, 0, 0, " Product not found.", null);
                     return badRequest(Json.toJson(response));
@@ -121,14 +125,14 @@ public class ProductMerchantController extends BaseController {
                     getProductMerchantDetail.update();
 
                     ProductMerchantDescription getProductMerchantDescription = ProductMerchantDescriptionRepository.findByProductMerchantDetail(getProductMerchantDetail);
-                    getProductMerchantDescription.setShortDescription(productRequest.getShortDescription());
-                    getProductMerchantDescription.setLongDescription(productRequest.getLongDescription());
+                    getProductMerchantDescription.setShortDescription(productRequest.getProductDescriptionRequest().getShortDescription());
+                    getProductMerchantDescription.setLongDescription(productRequest.getProductDescriptionRequest().getLongDescription());
                     getProductMerchantDescription.setProductMerchantDetail(getProductMerchantDetail);
                     getProductMerchantDescription.update();
 
                     trx.commit();
 
-                    response.setBaseResponse(1,offset, 1, success + " Product created successfully", null);
+                    response.setBaseResponse(1,offset, 1, success + " Product updated successfully", productMerchant.id);
                     return ok(Json.toJson(response));
 
                 } catch (Exception e) {
@@ -148,16 +152,131 @@ public class ProductMerchantController extends BaseController {
         return unauthorized(Json.toJson(response));
     }
 
+    public static Result getAllProduct(String filter, String sort, int offset, int limit, Boolean isActive) {
+        Merchant ownMerchant = checkMerchantAccessAuthorization();
+        if (ownMerchant != null) {
+            try {
+                Query<ProductMerchant> query = ProductMerchantRepository.findProductIsActiveAndMerchant(ownMerchant, isActive);
+                List<ProductMerchant> totalData = ProductMerchantRepository.getTotalDataPage(query);
+                List<ProductMerchant> productMerchants = ProductMerchantRepository.findProductWithPaging(query, sort, filter, offset, limit);
+                List<ProductResponse> productMerchantResponse = toResponses(productMerchants);
+                response.setBaseResponse(filter == null || filter.equals("") ? totalData.size() : productMerchantResponse.size(), offset, limit, success + " Showing data products", productMerchantResponse);
+                return ok(Json.toJson(response));
+            } catch (Exception e) {
+                Logger.info("Error: " + e.getMessage());
+            }
+        }
+        response.setBaseResponse(0, 0, 0, unauthorized, null);
+        return unauthorized(Json.toJson(response));
+    }
 
+    public static Result getProductById(Long id) {
+        Merchant ownMerchant = checkMerchantAccessAuthorization();
+        if (ownMerchant != null) {
+            try {
+                ProductMerchant productMerchant = ProductMerchantRepository.findById(id, ownMerchant);
+                if (productMerchant != null) {
+                    ProductResponse productResponse = toResponse(productMerchant);
+                    response.setBaseResponse(1, 0, 1, success + " Showing data product", productResponse);
+                    return ok(Json.toJson(response));
+                }
+            } catch (Exception e) {
+                Logger.info("Error: " + e.getMessage());
+            }
+        }
+        response.setBaseResponse(0, 0, 0, unauthorized, null);
+        return unauthorized(Json.toJson(response));
+    }
 
-
-
-
-
+    public static Result deleteProduct(Long id) {
+        Merchant ownMerchant = checkMerchantAccessAuthorization();
+        if (ownMerchant != null) {
+            try {
+                ProductMerchant productMerchant = ProductMerchantRepository.findById(id, ownMerchant);
+                if (productMerchant != null) {
+                    productMerchant.setIsActive(false);
+                    productMerchant.isDeleted = true;
+                    productMerchant.update();
+                    response.setBaseResponse(1, 0, 1, success + " Deleted data product", productMerchant.id);
+                    return ok(Json.toJson(response));
+                }
+            } catch (Exception e) {
+                Logger.info("Error: " + e.getMessage());
+            }
+        }
+        response.setBaseResponse(0, 0, 0, unauthorized, null);
+        return unauthorized(Json.toJson(response));
+    }
 
 
     // =============================================== construct =====================================================//
 
+    private static ProductResponse toResponse(ProductMerchant productMerchant) {
+        ProductResponse productResponse = new ProductResponse();
+        productResponse.setProductName(productMerchant.getProductName());
+        // ================================================================ //
+        CategoryMerchant categoryMerchant = CategoryMerchantRepository.findByIdAndMerchantId(productMerchant.getCategoryMerchant().id, productMerchant.getMerchant().id);
+        if (categoryMerchant != null) {
+            ProductResponse.CategoryResponse categoryResponse = ProductResponse.CategoryResponse.builder()
+                    .id(categoryMerchant.id)
+                    .categoryName(categoryMerchant.getCategoryName())
+                    .build();
+            productResponse.setCategory(categoryResponse);
+        }
+        SubCategoryMerchant subCategoryMerchant = SubCategoryMerchantRepository.findByIdAndMerchantId(productMerchant.getSubCategoryMerchant().id, productMerchant.getMerchant().id);
+        if (subCategoryMerchant != null) {
+            ProductResponse.SubCategoryResponse subCategoryResponse = ProductResponse.SubCategoryResponse.builder()
+                    .id(subCategoryMerchant.id)
+                    .subCategoryName(subCategoryMerchant.getSubcategoryName())
+                    .build();
+            productResponse.setSubCategory(subCategoryResponse);
+        }
+        BrandMerchant brandMerchant = BrandMerchantRepository.findByIdAndMerchantId(productMerchant.getBrandMerchant().id, productMerchant.getMerchant().id);
+        if (brandMerchant != null) {
+            ProductResponse.BrandResponse brandResponse = ProductResponse.BrandResponse.builder()
+                    .id(brandMerchant.id)
+                    .brandName(brandMerchant.getBrandName())
+                    .build();
+            productResponse.setBrand(brandResponse);
+        }
+        // ================================================================ //
+
+        ProductMerchantDetail productMerchantDetail = ProductMerchantDetailRepository.findByProduct(productMerchant);
+        if (productMerchantDetail != null) {
+            ProductDetailResponse productDetailResponse = ProductDetailResponse.builder()
+                    .productType(productMerchantDetail.getProductType())
+                    .isCustomizable(productMerchantDetail.getIsCustomizable())
+                    .productPrice(productMerchantDetail.getProductPrice())
+                    .discountType(productMerchantDetail.getDiscountType())
+                    .discount(productMerchantDetail.getDiscount())
+                    .productPriceAfterDiscount(productMerchantDetail.getProductPriceAfterDiscount())
+                    .productImageMain(productMerchantDetail.getProductImageMain())
+                    .productImage1(productMerchantDetail.getProductImage1())
+                    .productImage2(productMerchantDetail.getProductImage2())
+                    .productImage3(productMerchantDetail.getProductImage3())
+                    .productImage4(productMerchantDetail.getProductImage4())
+                    .build();
+            productResponse.setProductDetail(productDetailResponse);
+        }
+
+        ProductMerchantDescription productMerchantDescription = ProductMerchantDescriptionRepository.findByProductMerchantDetail(productMerchantDetail);
+        if (productMerchantDescription != null) {
+            ProductDescriptionResponse productDescriptionResponse = ProductDescriptionResponse.builder()
+                    .shortDescription(productMerchantDescription.getShortDescription())
+                    .longDescription(productMerchantDescription.getLongDescription())
+                    .build();
+            productResponse.setProductDescription(productDescriptionResponse);
+        }
+        return productResponse;
+    }
+
+    private static List<ProductResponse> toResponses(List<ProductMerchant> productMerchants) {
+        List<ProductResponse> productResponses = new ArrayList<>();
+        for (ProductMerchant productMerchant : productMerchants) {
+            productResponses.add(toResponse(productMerchant));
+        }
+        return productResponses;
+    }
 
     private static void constructProductEntityRequest(ProductMerchant newProductMerchant, Merchant merchant,
                                                       ProductRequest productRequest, CategoryMerchant categoryMerchant,
@@ -171,17 +290,17 @@ public class ProductMerchantController extends BaseController {
     }
 
     private static void constructProductDetailEntityRequest(ProductMerchantDetail newProductMerchantDetail, ProductMerchant newProductMerchant, ProductRequest productRequest) {
-        newProductMerchantDetail.setProductType(productRequest.getProductType());
-        newProductMerchantDetail.setIsCustomizable(productRequest.getIsCustomizable());
-        newProductMerchantDetail.setProductPrice(productRequest.getProductPrice());
-        newProductMerchantDetail.setDiscountType(productRequest.getDiscountType());
-        newProductMerchantDetail.setDiscount(productRequest.getDiscount());
-        newProductMerchantDetail.setProductPriceAfterDiscount(productRequest.getProductPriceAfterDiscount());
-        newProductMerchantDetail.setProductImageMain(productRequest.getProductImageMain());
-        newProductMerchantDetail.setProductImage1(productRequest.getProductImage1());
-        newProductMerchantDetail.setProductImage2(productRequest.getProductImage2());
-        newProductMerchantDetail.setProductImage3(productRequest.getProductImage3());
-        newProductMerchantDetail.setProductImage4(productRequest.getProductImage4());
+        newProductMerchantDetail.setProductType(productRequest.getProductDetailRequest().getProductType());
+        newProductMerchantDetail.setIsCustomizable(productRequest.getProductDetailRequest().getIsCustomizable());
+        newProductMerchantDetail.setProductPrice(productRequest.getProductDetailRequest().getProductPrice());
+        newProductMerchantDetail.setDiscountType(productRequest.getProductDetailRequest().getDiscountType());
+        newProductMerchantDetail.setDiscount(productRequest.getProductDetailRequest().getDiscount());
+        newProductMerchantDetail.setProductPriceAfterDiscount(productRequest.getProductDetailRequest().getProductPriceAfterDiscount());
+        newProductMerchantDetail.setProductImageMain(productRequest.getProductDetailRequest().getProductImageMain());
+        newProductMerchantDetail.setProductImage1(productRequest.getProductDetailRequest().getProductImage1());
+        newProductMerchantDetail.setProductImage2(productRequest.getProductDetailRequest().getProductImage2());
+        newProductMerchantDetail.setProductImage3(productRequest.getProductDetailRequest().getProductImage3());
+        newProductMerchantDetail.setProductImage4(productRequest.getProductDetailRequest().getProductImage4());
         newProductMerchantDetail.setProductMerchant(newProductMerchant);
     }
 
--- app/dtos/product/ProductRequest.java
+++ app/dtos/product/ProductRequest.java
@@ -5,9 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.validator.constraints.NotEmpty;
-
-import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @AllArgsConstructor
@@ -26,34 +23,12 @@ public class ProductRequest {
     private Long brandId;
 
     // ======== Detail ========== //
-    @JsonProperty("product_type")
-    private String productType;
-    @JsonProperty("is_customizable")
-    private Boolean isCustomizable;
-    @JsonProperty("product_price")
-    private BigDecimal productPrice;
-    @JsonProperty("discount_type")
-    private String discountType;
-    @JsonProperty("discount")
-    private Double discount;
-    @JsonProperty("product_price_after_discount")
-    private BigDecimal productPriceAfterDiscount;
-    @JsonProperty("product_image_main")
-    private String productImageMain;
-    @JsonProperty("product_image_1")
-    private String productImage1;
-    @JsonProperty("product_image_2")
-    private String productImage2;
-    @JsonProperty("product_image_3")
-    private String productImage3;
-    @JsonProperty("product_image_4")
-    private String productImage4;
+    @JsonProperty("product_detail")
+    private ProductDetailResponse productDetailRequest;
 
     // ======== Description ========== //
-    @JsonProperty("short_description")
-    private String shortDescription;
-    @JsonProperty("long_description")
-    private String longDescription;
+    @JsonProperty("product_description")
+    private ProductDescriptionResponse productDescriptionRequest;
 
 
 
