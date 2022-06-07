a116f4adb425be420eeb59dd1e2ece88afebd340
Fahmi Kudo <fahmi.hidayatullah12@gmail.com>
2022-06-08 06:28:43 +0700

    API bank account merchant

---
+++ app/dtos/bankaccount/BankAccountMerchantModel.java
@@ -0,0 +1,24 @@
+package dtos.bankaccount;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class BankAccountMerchantModel {
+
+    @JsonProperty("bank_name")
+    private String bankName;
+    @JsonProperty("account_number")
+    private String accountNumber;
+    @JsonProperty("account_name")
+    private String accountName;
+    @JsonProperty("is_primary")
+    private Boolean isPrimary;
+
+}
