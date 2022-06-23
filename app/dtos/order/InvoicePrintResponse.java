6c785237204c2ecf4d3c5c5be3c99fb80a804b55
Fahmi Kudo <fahmi.hidayatullah12@gmail.com>
2022-06-23 07:30:48 +0700

    add API print invoice and set session expire one hour

---
+++ app/dtos/order/InvoicePrintResponse.java
@@ -0,0 +1,66 @@
+package dtos.order;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import utils.BigDecimalSerialize;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class InvoicePrintResponse {
+
+    @JsonProperty("image_store_url")
+    private String imageStoreUrl;
+    @JsonProperty("store_name")
+    private String storeName;
+    @JsonProperty("store_address")
+    private String storeAddress;
+    @JsonProperty("store_phone_number")
+    private String storePhoneNumber;
+
+    @JsonProperty("invoice_number")
+    private String invoiceNumber;
+    @JsonProperty("order_number")
+    private String orderNumber;
+    @JsonProperty("order_type")
+    private String orderType;
+    @JsonProperty("order_date")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy", timezone = "Asia/Jakarta")
+    private Date orderDate;
+    @JsonProperty("order_time")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss", timezone = "Asia/Jakarta")
+    private Date orderTime;
+    @JsonProperty("order_detail")
+    private List<OrderDetailResponse> orderDetails;
+
+    @JsonProperty("sub_total")
+    @JsonSerialize(using = BigDecimalSerialize.class)
+    private BigDecimal subTotal;
+    @JsonProperty("tax")
+    @JsonSerialize(using = BigDecimalSerialize.class)
+    private BigDecimal taxPrice;
+    @JsonProperty("service")
+    @JsonSerialize(using = BigDecimalSerialize.class)
+    private BigDecimal paymentFeeOwner;
+    @JsonProperty("service_fee")
+    @JsonSerialize(using = BigDecimalSerialize.class)
+    private BigDecimal paymentFeeCustomer;
+    @JsonProperty("total")
+    @JsonSerialize(using = BigDecimalSerialize.class)
+    private BigDecimal total;
+
+    @JsonProperty("order_queue")
+    private Integer orderQueue;
+
+
+
+
+}
