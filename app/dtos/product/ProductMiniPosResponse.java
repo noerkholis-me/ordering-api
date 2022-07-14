6fa71dbabf4b4cb6a9c5bd947201f9c481dc6501
rioagungfirmansyah <rioagungfirmansyah@gmail.com>
2022-07-14 16:49:12 +0700

    Added product list filter by category, store, and pagination for MiniPos :zap:

--- app/dtos/product/ProductMiniPosResponse.java
+++ app/dtos/product/ProductMiniPosResponse.java
@@ -2,15 +2,21 @@ package dtos.product;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import lombok.*;
+import dtos.category.SubsCategoryMerchantResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import utils.BigDecimalSerialize;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter @Setter
-public class ProductKiosKResponse implements Serializable {
+@Getter
+@Setter
+public class ProductMiniPosResponse implements Serializable {
 
     @JsonProperty("product_id")
     private Long productId;
@@ -26,31 +32,33 @@ public class ProductKiosKResponse implements Serializable {
 
     @JsonProperty("product_type")
     private String productType;
-    
+
     @JsonProperty("is_customizable")
     private Boolean isCustomizable;
-    
+
     @JsonProperty("product_price")
     @JsonSerialize(using = BigDecimalSerialize.class)
     private BigDecimal productPrice;
-    
+
     @JsonProperty("discount_type")
     private String discountType;
-    
+
     @JsonProperty("discount")
     private Double discount;
-    
+
     @JsonProperty("product_price_after_discount")
     @JsonSerialize(using = BigDecimalSerialize.class)
     private BigDecimal productPriceAfterDiscount;
-    
+
     @JsonProperty("product_image_main")
     private String productImageMain;
-    
+
     @JsonProperty("short_description")
     private String shortDescription;
-    
+
     @JsonProperty("long_description")
     private String longDescription;
 
+    @JsonProperty("category")
+    private ProductPosCategoryResponse category;
 }
