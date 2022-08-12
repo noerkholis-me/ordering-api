8bdb5f6551e0544a3c868f227cbdccf6a7f82510
Fahmi Kudo <fahmi.hidayatullah12@gmail.com>
2022-08-12 15:37:55 +0700

    mapping status order for queue

--- app/models/transaction/OrderStatus.java
+++ app/models/transaction/OrderStatus.java
@@ -19,4 +19,26 @@ public enum OrderStatus {
         return this.status;
     }
 
+    public static OrderStatus convertToOrderStatus(String status) {
+        if (status == null) {
+            return null;
+        } else if (status.equalsIgnoreCase("NEW_ORDER")) {
+            return OrderStatus.NEW_ORDER;
+        } else if (status.equalsIgnoreCase("PROCESS")) {
+            return OrderStatus.PROCESS;
+        } else if (status.equalsIgnoreCase("READY_TO_PICKUP")) {
+            return OrderStatus.READY_TO_PICKUP;
+        } else if (status.equalsIgnoreCase("DELIVERY")) {
+            return OrderStatus.DELIVERY;
+        } else if (status.equalsIgnoreCase("CLOSED")) {
+            return OrderStatus.CLOSED;
+        } else if (status.equalsIgnoreCase("PENDING")) {
+            return OrderStatus.PENDING;
+        } else if (status.equalsIgnoreCase("CANCELED")) {
+            return OrderStatus.CANCELED;
+        } else {
+            return null;
+        }
+    }
+
 }
