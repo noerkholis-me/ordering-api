f5acfc0d4cb31bfc4f44140765971aaab0520e10
bangbang <bangbang.bangbang@appschef.com>
2022-10-06 15:19:30 +0700

    fixing webhook shipper save

--- app/controllers/users/WebhookShipperController.java
+++ app/controllers/users/WebhookShipperController.java
@@ -168,7 +168,7 @@ public class WebhookShipperController extends BaseController {
             statusOrder.status = tmpStatusName;
 
             statusOrder.notes = tmpDescription;
-            statusOrder.order = objOrder;
+//            statusOrder.order = objOrder;
             statusOrder.order_id = objOrder.id;
             statusOrder.save();
 
