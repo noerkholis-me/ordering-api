3b16dde850b3e327ec76da20ad0419cd7241b0bd
bangbang <bangbang.bangbang@appschef.com>
2022-10-06 09:33:05 +0700

    change query evolution

--- conf/evolutions/default/138.sql
+++ conf/evolutions/default/138.sql
@@ -5,7 +5,7 @@ create table shipper_order_status (
   is_deleted                boolean,
   order_id                  bigint,
   status                    varchar(255),
-  user_id            		bigint,
+  notes                     varchar(255),
   created_at                timestamp not null,
   updated_at                timestamp not null,
   constraint pk_shipper_order_status primary key (id))
@@ -13,11 +13,8 @@ create table shipper_order_status (
 
 create sequence shipper_order_status_seq;
 
-alter table shipper_order_status add constraint fk_shipper_order_status_orders foreign key (order_id) references orders (id);
-create index ix_shipper_order_status_orders on shipper_order_status (order_id);
-
-alter table shipper_order_status add constraint fk_shipper_order_status_user_cms foreign key (user_id) references user_cms (id);
-create index ix_shipper_order_status_user_cms on s_order_status (user_id);
+alter table shipper_order_status add constraint fk_s_shipper_order_status_orders foreign key (order_id) references orders (id);
+create index ix_s_shipper_order_status_orders on shipper_order_status (order_id);
 
 
 # --- !Downs
--- conf/evolutions/default/139.sql
+++
@@ -1,6 +0,0 @@
-# --- !Ups
-ALTER TABLE shipper_order_status ADD column notes varchar(200) default null;
-
-
-# --- !Downs
-ALTER TABLE shipper_order_status DROP column if exists notes;
