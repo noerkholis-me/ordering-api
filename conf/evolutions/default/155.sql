593294e30eef026613441d807cb6cb25882189d4
MichaelPatrick-19910 <110223040+MichaelPatrick-19910@users.noreply.github.com>
2023-04-28 14:29:44 +0700

    update sql files

--- conf/evolutions/default/155.sql
+++ conf/evolutions/default/155.sql
@@ -18,29 +18,12 @@ CREATE TABLE voucher_merchant_new (
     constraint pk_voucher_merchant_new primary key (id)
 );
 
-CREATE TABLE voucher_how_to_use (
-    id                          bigint not null,
-    is_deleted                  boolean,
-    content                     text,
-    created_at                  timestamp not null,
-    updated_at                  timestamp not null,
-    voucher_merchant_id         bigint not null,
-    constraint pk_voucher_how_to_use primary key (id)
-);
-
-
-
 alter table voucher_merchant_new add constraint fk_voucher_merchant_new_merchant_id foreign key (merchant_id) references merchant (id);
-alter table voucher_how_to_use add constraint fk_voucher_how_to_use_voucher_id foreign key (voucher_merchant_id) references voucher_merchant_new (id);
 create index ix_voucher_merchant_new_merchant_id on voucher_merchant_new (merchant_id);
-create index ix_voucher_how_to_use_voucher_id on voucher_how_to_use (voucher_merchant_id);
 
 create sequence voucher_merchant_new_seq;
-create sequence voucher_how_to_use_seq;
 
 # --- !Downs
 
 drop table if exists voucher_merchant_new cascade;
 drop sequence if exists voucher_merchant_new_seq;
-drop table if exists voucher_how_to_use cascade;
-drop sequence if exists voucher_how_to_use_seq;
