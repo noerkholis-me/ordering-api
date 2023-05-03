eae61078dd3ac282e642aab7fa39cf9361a9a880
MichaelPatrick-19910 <110223040+MichaelPatrick-19910@users.noreply.github.com>
2023-05-03 14:40:23 +0700

    add voucher_user table and modify order table sql

---
+++ conf/evolutions/default/158.sql
@@ -0,0 +1,26 @@
+# --- !Ups
+
+create table voucher_user (
+	id				bigint not null,
+	is_deleted		boolean,
+	user_id			bigint not null,
+	voucher_id		bigint not null,
+	status			varchar(30),
+	constraint pk_voucher_user primary key (id)
+);
+alter table voucher_user add constraint fk_voucher_user_user_id foreign key (user_id) references "member" (id);
+alter table voucher_user add constraint fk_voucher_user_voucher_id foreign key (voucher_id) references voucher_merchant_new (id);
+
+create index ix_voucher_user_voucher_id on voucher_user (voucher_id);
+create index ix_voucher_user_user_id on voucher_user (user_id);
+
+create sequence voucher_user_seq;
+
+alter table orders add discount_amount numeric;
+
+# --- !Downs
+
+drop table if exists voucher_user cascade;
+drop sequence if exists voucher_user_seq;
+
+alter table orders drop column if exists discount_amount;
