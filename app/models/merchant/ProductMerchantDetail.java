35ca98f346adcee73cab4cb9efb73baf7c723c1c
Indra Mulyawan <indra.mulyawan@smt-id.com>
2023-11-20 08:55:46 +0700

    feat: add API product

--- app/models/merchant/ProductMerchantDetail.java
+++ app/models/merchant/ProductMerchantDetail.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import models.BaseModel;
+import models.Store;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -67,6 +68,8 @@ public class ProductMerchantDetail extends BaseModel {
 
     @Transient
     public Long total_penjualan;
+
+    public static Finder<Long, ProductMerchantDetail> find = new Finder<Long, ProductMerchantDetail>(Long.class, ProductMerchantDetail.class);
 
     public ProductMerchantDetail(ProductMerchant productMerchant, ProductRequest productRequest) {
         this.setProductType(productRequest.getProductDetailRequest().getProductType());
