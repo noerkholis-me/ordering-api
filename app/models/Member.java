51a533ffee35a4b38161d1a9e09ed88d799cbbc2
Asep Wilayana <asep.wilayana@smt-id.com>
2024-01-25 14:16:17 +0700

    🔨  fix get member

--- app/models/Member.java
+++ app/models/Member.java
@@ -204,7 +204,7 @@ public class Member extends BaseModel {
     public static Finder<Long, Member> find = new Finder<Long, Member>(Long.class, Member.class);
 
     public static Member findDataCustomer(String email, String phoneNumber){
-        return find.where().raw("t0.email = '" + email + "' or t0.phone = '" + phoneNumber + "'").eq("t0.is_deleted", false).findUnique();
+        return find.where().raw("t0.email = '" + email + "' AND t0.phone = '" + phoneNumber + "'").eq("t0.is_deleted", false).findUnique();
     }
 
     public static Member findDataCustomerByName(String name, String phoneNumber){
