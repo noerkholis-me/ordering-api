d794358b1cc1733db021b77aca1bc715db210421
Asep Wilayana <asep.wilayana@smt-id.com>
2024-02-02 13:50:09 +0700

    🔨 update query banner bazar

--- conf/evolutions/default/167.sql
+++ conf/evolutions/default/167.sql
@@ -1,6 +1,6 @@
 # --- !Ups
 
-create table banner_bazar(
+create table banner_bazaar(
     id                        bigint not null,
     is_deleted                boolean,
     title                     varchar(255),
@@ -8,11 +8,11 @@ create table banner_bazar(
     url                       varchar(255),
     created_at                timestamp not null,
     updated_at                timestamp not null,
-    constraint pk_banner_bazar primary key (id)
+    constraint pk_banner_bazaar primary key (id)
 );
 
-create sequence banner_bazar_seq;
+create sequence banner_bazaar_seq;
 
 
 # --- !Downs
-drop sequence if exists banner_bazar_seq;
+drop sequence if exists banner_bazaar_seq;
