66b8b286e6173bfd2dab82c39ec42ee39cc6e37e
Asep Wilayana <asep.wilayana@smt-id.com>
2024-04-16 17:36:50 +0700

    change exist to exists

--- conf/evolutions/default/166.sql
+++ conf/evolutions/default/166.sql
@@ -5,5 +5,5 @@ alter table order_payment add column delivery_fee                  numeric;
 
 # --- !Downs
 
-alter table order_payment drop column if exist delivery_fee;
+alter table order_payment drop column if exists delivery_fee;
 
