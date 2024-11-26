62d6d1e69a59522b87415f1c253930e4af713743
Ahmad Mughni <ahmad.mughni@smt-id.com>
2024-11-26 15:50:53 +0700

    :bug: fixing bug sometimes cannot find orderDetail

--- app/repository/OrderDetailRepository.java
+++ app/repository/OrderDetailRepository.java
@@ -1,5 +1,6 @@
 package repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import models.transaction.Order;
@@ -14,5 +15,9 @@ public class OrderDetailRepository extends Model {
   // Added method to find OrderDetail by ID
   public static Optional<OrderDetail> findById(Long id) {
     return Optional.ofNullable(find.byId(id));
+  }
+
+  public static List<OrderDetail> findByOrderId(Long orderId) {
+    return find.where().eq("order.id", orderId).findList(); // Return a list of OrderDetail
   }
 }
